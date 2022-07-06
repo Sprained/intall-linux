7990e30e4d803bcc203190a9f93187c48eadeb7b
Gabriel Resende <gabriel.resende@terceiroslinx.com.br>
2022-07-06 13:36:13 -0300

    fix nvm

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,11 +6,11 @@ sudo apt-get update -y
 sudo apt-get install curl -y
 
 # -------------------------------- Node ------------------------------ #
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 echo "Instalando nvm, node e yarn"
 if [ -f ~/.nvm/nvm.sh ]; then
     echo 'sourcing nvm from ~/.nvm'
     . ~/.nvm/nvm.sh
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash -y
     nvm install node
     npm install --global yarn
 fi
